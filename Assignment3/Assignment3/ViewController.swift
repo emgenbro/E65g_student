92c9e4c56ebdc481384671e5ba4f0d41a3c7d528
Damon Emgenbroich <emgenbro@yahoo.com>
2017-03-25 14:43:04 -0400

    Added Button

--- Assignment3/Assignment3/ViewController.swift
+++ Assignment3/Assignment3/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var stepButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +22,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //IBAction func nextGridState(_ sender : Any){
+        //nextGridState(sender)
+    //}
 
-
+    
 }
 
