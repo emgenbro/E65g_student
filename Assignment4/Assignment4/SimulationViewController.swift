76425e5e5f51663207842536ffec23f4b1d79271
Damon Emgenbroich <emgenbro@yahoo.com>
2017-04-12 21:18:43 -0400

    complete q1

---
+++ Assignment4/Assignment4/SimulationViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SimulationViewController.swift
+//  Assignment4
+//
+//  Created by Damon Emgenbroich on 4/12/17.
+//  Copyright © 2017 Harvard Division of Continuing Education. All rights reserved.
+//
+
+import UIKit
+
+class SimulationViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
